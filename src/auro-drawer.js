a5336089df99ac8c8ff645036d1296c490fd67d5
Jordan Jones <jmjones2463@gmail.com>
2024-01-23 10:46:23 -0800

    perf(src): implement all component src code

--- src/auro-drawer.js
+++ src/auro-drawer.js
@@ -1,65 +1,48 @@
-// Copyright (c) 2024 Alaska Airlines. All right reserved. Licensed under the Apache-2.0 license
+// Copyright (c) 2020 Alaska Airlines. All right reserved. Licensed under the Apache-2.0 license
 // See LICENSE in the project root for license information.
 
 // ---------------------------------------------------------------------
 
-// If using litElement base class
-import { LitElement, html } from "lit";
-
-// If using auroElement base class
-// See instructions for importing auroElement base class https://git.io/JULq4
-// import { LitElement, html } from "lit";
-// import AuroElement from '@aurodesignsystem/webcorestylesheets/dist/auroElement/auroElement';
+// import { html } from "lit-element";
+// import { classMap } from 'lit-html/directives/class-map';
+import ComponentBase from './componentBase';
 
 // Import touch detection lib
 import styleCss from "./style-css.js";
 
 // See https://git.io/JJ6SJ for "How to document your components using JSDoc"
 /**
- * The auro-drawer element provides users a way to ... (it would be great if you fill this out).
- *
- * @attr {Boolean} fixed - Uses fixed pixel values for element shape
- * @attr {String} cssClass - Applies designated CSS class to demo element - you want to delete me!
+ * @attr {Boolean} left - Sets dialog box to open from the left
  */
 
 // build the component class
-export class AuroDrawer extends LitElement {
+export class AuroDrawer extends ComponentBase {
   // constructor() {
   //   super();
   // }
 
-  // This function is to define props used within the scope of this component
-  // Be sure to review  https://lit.dev/docs/components/properties/
-  // to understand how to use reflected attributes with your property settings.
+  // function to define props used within the scope of this component
   static get properties() {
     return {
-      // ...super.properties,
-
-      // this property is DEMO ONLY! Please delete.
-      cssClass:   { type: String }
+      ...super.properties,
+      // use custom accessors on base class
+      open: {
+        ...super.properties.open,
+        noAccessor: true
+      }
     };
   }
 
   static get styles() {
-    return [styleCss];
-  }
-
-  // When using auroElement, use the following attribute and function when hiding content from screen readers.
-  // aria-hidden="${this.hideAudible(this.hiddenAudible)}"
-
-  // function that renders the HTML and CSS into  the scope of the component
-  render() {
-    return html`
-
-      <!-- this is demo code, DO NOT USE IN YOUR ELEMENT -->
-      <div class=${this.cssClass} tabindex="0">
-        <slot></slot>
-      </div>
-    `;
+    return [
+      super.styles,
+      styleCss,
+    ];
   }
 }
 
-// default internal definition
+/* istanbul ignore else */
+// define the name of the custom component
 if (!customElements.get("auro-drawer")) {
   customElements.define("auro-drawer", AuroDrawer);
 }

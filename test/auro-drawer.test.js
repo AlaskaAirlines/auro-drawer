45c78c027216df24c13b48a9e50b3944801512e1
Eunsun Mota <eunsun.mota@alaskaair.com>
2025-03-14 14:38:23 -0400

    test: update test file to support new drawer

--- test/auro-drawer.test.js
+++ test/auro-drawer.test.js
@@ -1,4 +1,5 @@
 import { fixture, html, expect, oneEvent, elementUpdated } from '@open-wc/testing';
+import { setViewport } from '@web/test-runner-commands';
 import '../index.js';
 
 describe('auro-drawer', () => {
@@ -14,10 +15,79 @@ describe('auro-drawer', () => {
     await expect(el).to.be.accessible();
   });
 
+  it('auro-drawer gets open by triggerElement', async () => {
+    const el = await fixture(html`
+      <div>
+        <auro-drawer></auro-drawer>
+        <button> Open Drawer</button>
+      </div>
+    `);
+
+    const drawer = el.querySelector('auro-drawer');
+    const button = el.querySelector('button');
+    drawer.triggerElement = button;
+    await elementUpdated(drawer); // wait drawer setup the trigger event handlers
+
+    button.click();
+    await elementUpdated(drawer);
+
+    await expect(drawer.hasAttribute('open')).to.be.true;
+  });
+
+  it('nested auro-drawer has same width and height as its parent', async () => {
+    const el = await fixture(html`
+      <div style="width: 200px; height: 200px">
+        <auro-drawer open="true" nested>
+          <span slot="header">Blocking drawer</span>
+          <span slot="content">Hello World!</span>
+          <span slot="footer"><button>Click</button></span>
+        </auro-drawer>
+      </div>
+    `);
+    const drawer = el.children[0];
+    await expect(drawer.bib.offsetWidth).to.be.equal(el.offsetWidth);
+    await expect(drawer.bib.offsetHeight).to.be.equal(el.offsetHeight);
+  });
+
   it('auro-drawer custom element is defined', async () => {
     const el = await !!customElements.get("auro-drawer");
 
     await expect(el).to.be.true;
+  });
+
+  it('auro-drawer size gets carried down to bib', async () => {
+    const el = await fixture(html`
+      <auro-drawer size="sm"></auro-drawer>
+    `);
+    await expect(el.drawerBib.getAttribute('size')).to.equal('sm');
+  });
+
+  it('auro-drawer placement gets carried down to bib', async () => {
+    const el = await fixture(html`
+      <auro-drawer placement="left"></auro-drawer>
+    `);
+    await expect(el.drawerBib.getAttribute('placement')).to.equal('left');
+  });
+
+  it('unformatted auro-drawer does not render header or footer', async () => {
+    const el = await fixture(html`
+      <auro-drawer unformatted></auro-drawer>
+    `);
+
+    const wrapper = el.drawerBib.shadowRoot.querySelector('.wrapper');
+    await expect(wrapper.querySelector('.content')).to.not.equal(null);
+    await expect(wrapper.querySelector('.header')).to.equal(null);
+    await expect(wrapper.querySelector('.footer')).to.equal(null);
+  });
+
+  it('auro-drawer renders a close icon', async () => {
+    const el = await fixture(html`
+      <auro-drawer></auro-drawer>
+    `);
+
+    const root = el.drawerBib;
+    const title = root.shadowRoot.querySelector('#closeButton');
+    await expect(title).to.not.equal(null);
   });
 
   it('auro-drawer modal-drawer does not render a close icon', async () => {
@@ -25,8 +95,8 @@ describe('auro-drawer', () => {
       <auro-drawer modal></auro-drawer>
     `);
 
-    const root = el.shadowRoot;
-    const title = root.querySelector('#drawer-close');
+    const root = el.drawerBib;
+    const title = root.shadowRoot.querySelector('#closeButton');
     await expect(title).to.equal(null);
   });
 
@@ -38,9 +108,9 @@ describe('auro-drawer', () => {
       </auro-drawer>
     `);
   
-    window.dispatchEvent(new KeyboardEvent('keydown', { key: 'Escape' }));
+    document.dispatchEvent(new KeyboardEvent('keydown', { key: 'Escape' }));
     await elementUpdated(el);
-    expect(el.open).to.be.false;
+    expect(el.hasAttribute('open')).to.be.false;
   })
   
   it('does not close when ESC pressed if modal', async () => {
@@ -53,7 +123,7 @@ describe('auro-drawer', () => {
   
     window.dispatchEvent(new KeyboardEvent('keydown', { key: 'Escape' }));
     await elementUpdated(el);
-    expect(el.open).to.be.true;
+    expect(el.hasAttribute('open')).to.be.true;
   })
 
   it('dispatches toggle event on close', async () => {
@@ -64,7 +134,7 @@ describe('auro-drawer', () => {
       </auro-drawer>
     `);
 
-    el.open = false;
+    el.removeAttribute('open');
     await oneEvent(el, 'toggle');
   });
 
@@ -76,8 +146,40 @@ describe('auro-drawer', () => {
       </auro-drawer>
     `);
 
-    el.open = false;
+    el.removeAttribute('open');
     await el.updateComplete;
+  });
+
+  it('sm auro-drawer is smaller than md auro-drawer', async () => {
+    const el = await fixture(html`
+      <div>
+        <auro-drawer size="sm" open></auro-drawer>
+        <auro-drawer size="md" open></auro-drawer>
+      </div>
+    `);
+    const sm = el.children[0].drawerBib.shadowRoot.querySelector('.wrapper');
+    const md = el.children[1].drawerBib.shadowRoot.querySelector('.wrapper');
+
+    expect(sm.offsetWidth).to.be.lessThan(md.offsetWidth);
+  });
+
+  it('auro-drawer covers the fullscreen in mobile view', async () => {
+    await setViewport({
+      width: 300,
+      height: 800
+    });
+
+    const el = await fixture(html`
+      <auro-drawer open>
+        <span slot="header">Blocking drawer</span>
+        <span slot="content">Hello World!</span>
+        <span slot="footer"><button>Click</button></span>
+      </auro-drawer>
+    `);
+    await expect(el.drawerBib.hasAttribute('stretch')).to.be.true;
+    const contentWrapper = el.drawerBib.shadowRoot.querySelector('.wrapper');
+    await expect(contentWrapper.offsetWidth).to.be.equal(visualViewport.width);
+    await expect(contentWrapper.offsetHeight).to.be.equal(visualViewport.height);
   });
 });
 
@@ -87,7 +189,7 @@ function sleep(ms) {
 
 async function getFixtureWithOpenButton() {
   const open = function() {
-    el.open = true;
+    el.setAttribute('open', '');
   }
   const el = await fixture(html`
     <auro-drawer>

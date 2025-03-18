62c988c0c4c9f8413b8b7829c77cecf3e757562b
Eunsun Mota <eunsun.mota@alaskaair.com>
2025-03-18 12:38:47 -0400

    docs: update api doc

---
+++ scripts/wca/auro-drawer.js
@@ -0,0 +1,11 @@
+
+import { AuroDrawer } from '../../src/auro-drawer.js';
+
+/**
+     * @private
+     */
+class AuroDrawerWCA extends AuroDrawer {}
+
+if (!customElements.get("auro-drawer")) {
+  customElements.define("auro-drawer", AuroDrawerWCA);
+}

ea4ef5fb1418ada3281300cb47c6908147fae808
Eunsun Mota <eunsun.mota@alaskaair.com>
2025-04-16 12:33:08 -0400

    docs: update some examples to test `trigger` event handlers

--- apiExamples/basic.js
+++ apiExamples/basic.js
@@ -2,13 +2,8 @@ export function initBasicExample() {
   const buttons = [document.querySelector('#openBasic'), document.querySelector('#closeBasic')];
   const drawer = document.querySelector('#defaultDrawer');
 
-  buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-      if (drawer.hasAttribute('open')) {
-        drawer.removeAttribute('open');
-      } else {
-        drawer.setAttribute('open', true);
-      }
-    });
+  drawer.triggerElement = buttons[0];
+  buttons[1].addEventListener('click', () => {
+    drawer.removeAttribute('open');
   });
 }
